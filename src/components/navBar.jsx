fd06dc01b2a7e47762f09f45f227c190c18d8b1e
Blake Spears <blake.g.spears@gmail.com>
2022-10-25 00:27:59 -0500

    STARTED CSS

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -16,7 +16,7 @@ function NavBar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-expand-md bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" href="/#">
           Ergononics
